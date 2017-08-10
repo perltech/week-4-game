43c487dabe3932ef61c09eb180efaa5b27d1f84d
perltech <noah.perlmutter@gmail.com>
2017-08-10 15:32:39 -0600

    add button logic

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -10,31 +10,76 @@ $(document).ready(function(){
 
 	var randValue = Math.floor(Math.random() * 102) + 19;
 
-	function randGem() {
-		var randGem = Math.floor(Math.random() * 12) + 1;
-	}
+	// function randGem() {
+	// 	var randGem = Math.floor(Math.random() * 12) + 1;
+	// }
 
 
 	var gem = {
-	// 	rand: function() {
-	// 		var rand = Math.floor(Math.random() * 12) + 1;
-	// 		return rand;
-	// },
+		rand: function() {
+			var rand = Math.floor(Math.random() * 12) + 1;
+			return rand;
+	},
 
-
+		//Object to create random number for onclick
 		redGem: function(rand) {
 			//var randGem = Math.floor(Math.random() * 12) + 1;
+			var localGem = gem.rand();
+			var storedGem = $("#redGem").val(localGem);	// How do I capture that random value into a variable?
+			gemPoints += storedGem;
+			$("#userPoints").html(gemPoints);
+			// console.log(gemPoints);
+		},
+
+		blueGem: function(rand) {
 			var localGem = gem.rand();	// How do I capture that random value into a variable?
 			gemPoints += localGem;
-			$("#targetValue").html(gemPoints);
-			console.log(gemPoints);
+			$("#userPoints").html(gemPoints);
+			// console.log(gemPoints);
+		},
+
+		yellowGem: function(rand) {
+			var localGem = gem.rand();	// How do I capture that random value into a variable?
+			gemPoints += localGem;
+			$("#userPoints").html(gemPoints);
+			// console.log(gemPoints);
 		}, 
+
+		greenGem: function(rand) {
+			var localGem = gem.rand();	// How do I capture that random value into a variable?
+			gemPoints += localGem;
+			$("#userPoints").html(gemPoints);
+			// console.log(gemPoints);
+		}
+
+	} // end of gem object
+
+	// Fetch random value for player to match
+	$("#targetValue").html(randValue);
+
+	// Change to swith statement Switch($("button")) and then each condition targets "#*Gem"
+	// $("#redGem").click(gem.redGem);
+	// $("#blueGem").click(gem.blueGem);
+	// $("#yellowGem").click(gem.yellowGem);
+	// $("#greenGem").click(gem.greenGem);
+	console.log(gemPoints);
+	console.log(randValue);
+
+
+	// If statement to control win condition
+	if (gemPoints < randValue) {
+		$("#redGem").click(gem.redGem);
+		$("#blueGem").click(gem.blueGem);
+		$("#yellowGem").click(gem.yellowGem);
+		$("#greenGem").click(gem.greenGem);
 	}
-
-
-
-	//Change to swith statement Switch($("button")) and then each condition targets "#*Gem"
-	$("#redGem").click(gem.redGem);
-	console.log(gem.redGem);
+	else if (gemPoints === randValue) {
+		win++;
+		alert("Victory is yours!");
+	}
+	else if (gemPoints > randValue) {
+		losses++;
+		alert("YOU LOSE!!!");
+	}
 
 });	// Ready Function
