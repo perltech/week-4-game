0b3003ddc7fb0975068f2c1066aa3b6a7b46d4ed
perltech <noah.perlmutter@gmail.com>
2017-08-13 13:39:07 -0600

    Properly capture random gem values

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -6,51 +6,50 @@ $(document).ready(function(){
 
 	var gemPoints = 0; // Variable for aggregated points from clicks on gems
 
-	//var randGem = Math.floor(Math.random() * 12) + 1;
+	
 
 	var randValue = Math.floor(Math.random() * 102) + 19;
 
-	// function randGem() {
-	// 	var randGem = Math.floor(Math.random() * 12) + 1;
-	// }
 
+	function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	function runGame() {
+		// Logic to control game starting and stopping
+	}
+
+	var randGemRed = getRandomInt(1,12);
+	var randGemBlue = getRandomInt(1,12);
+	var randGemYellow = getRandomInt(1,12);
+	var randGemGreen = getRandomInt(1,12);
 
 	var gem = {
-		rand: function() {
-			var rand = Math.floor(Math.random() * 12) + 1;
-			var randLink = parseInt(rand);
-			return randLink;
-	},
+		
 
-		//Object to create random number for onclick
 		redGem: function(rand) {
-			//var randGem = Math.floor(Math.random() * 12) + 1;
-			var localGem = gem.rand();
-			//var storedGem = $("#redGem").val(localGem);	// How do I capture that random value into a variable?
+			var localGem = randGemRed;
 			gemPoints +=  localGem;
 			$("#userPoints").html(gemPoints);
-			// console.log(gemPoints);
+			
 		},
 
 		blueGem: function(rand) {
-			var localGem = gem.rand();	// How do I capture that random value into a variable?
+			var localGem = randGemBlue;
 			gemPoints += localGem;
 			$("#userPoints").html(gemPoints);
-			// console.log(gemPoints);
 		},
 
 		yellowGem: function(rand) {
-			var localGem = gem.rand();	// How do I capture that random value into a variable?
+			var localGem = randGemYellow;
 			gemPoints += localGem;
 			$("#userPoints").html(gemPoints);
-			// console.log(gemPoints);
 		}, 
 
 		greenGem: function(rand) {
-			var localGem = gem.rand();	// How do I capture that random value into a variable?
+			var localGem = randGemGreen;
 			gemPoints += localGem;
 			$("#userPoints").html(gemPoints);
-			// console.log(gemPoints);
 		}
 
 	} // end of gem object
